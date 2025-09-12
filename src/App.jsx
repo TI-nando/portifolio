e606ab2d2a165271e7f971c59c76c1f1efa6e887
Fernando Henrique <fernandoagro.654@gmail.com>
2025-09-12 19:13:33 -0300

    resolvido problema de comflito

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from 'react'
-<<<<<<< HEAD
 import Header from './components/Header/Header'
 import Hero from './components/Hero/Hero'
 import About from './components/About/About'
@@ -8,16 +7,6 @@ import Skills from './components/Skills/Skills'
 import Education from './components/Education/Education'
 import Footer from './components/Footer/Footer'
 import WhatsAppButton from './components/WhatsAppButton/WhatsAppButton'
-=======
-import Header from './components/Header'
-import Hero from './components/Hero'
-import About from './components/About'
-import Projects from './components/Projects'
-import Skills from './components/Skills'
-import Education from './components/Education'
-import Footer from './components/Footer'
-import WhatsAppButton from './components/WhatsAppButton'
->>>>>>> 7e1adcfb52e2d6560d8cba620c02218a7b07a6a6
 
 function App() {
   const [activeSection, setActiveSection] = useState('home')
